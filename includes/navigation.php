eca08fcb75d1e8b5ec450d4d8bb45f377dbd6505
CharlZ <charles.ceo2020@gmail.com>
2020-04-29 11:07:31 -0600

    concatenation fuction and navigation file added

---
+++ includes/navigation.php
@@ -0,0 +1,9 @@
+<nav>
+    <h2> Website Navigation </h2>
+    <ul>
+        <li>
+            <a href="./index.php">Home</a>
+        </li>
+    </ul>
+ </nav>
+ <?php // By default, anything written in a php file will be sent to the browser (or whatever client  made the request) See above, we have some HTML outputting as we did not include any php tags (<?php etc) that would tell php otherwise
--- index.php
+++ index.php
@@ -1,48 +1,80 @@
 <?php
-    // Basic assignment
-    $myMessage = 'My first PHP!';
-    # This is also a comment!
-    /**
-     * Multi-line
-     * comment
-     * yaaaaay!!!!!!
-     */
-    /**
-     * Data-types
-     */
-    //string
-    $myString = 'Hello Charles';
-    //Integer
-    $myInteger = 37;
-    // float/double
-    $myFloat = 3.825;
-    // Boolean
-    $myBoolTrue = TRUE;
-    $myBoolFALSE = FALSE;
-    // nULL
-    $myNull = NULL;
-    // object
-    $myObject = new stdClass();
-    // Array
-    $myArray = [ 'First item', 2, 'seventh item' ];
-    $myOtherArray = array (
-        $myString,
-        $myInteger,
-        $myFloat,
-        $myBoolTrue,
-        $myBoolFALSE,
-        $myNull,
-        $myObject,
-        $myArray
-    );
+  // Basic assignment.
+  $myMessage = 'My First PHP!';
+  # This is also a comment!
+  /*
+    Multi-line
+        comments
+      yay!
+   */
+
+  /**
+   * Data-Types.
+   */
+  // String.
+  $myString = 'Hello, World!';
+  // Integer.
+  $myInteger = 34;
+  // Float / Double.
+  $myFloat = 3.14;
+  // Boolean.
+  $myBoolTrue = TRUE;
+  $myBoolFalse = false;
+  // Null.
+  $myNull = NULL;
+  // Object.
+  $myObject = new stdClass();
+  // Array.
+  $myArray = [ 'First Item', 2, 'third item' ];
+  $myOtherArray = array(
+    $myString,
+    $myInteger,
+    $myFloat,
+    $myBoolTrue,
+    $myBoolFalse,
+    $myNull,
+    $myObject,
+    $myArray
+  );
+  /**
+   * Strings.
+   */
+  $string1 = 'Hello, my name is ';
+  $string2 = 'Michael Jackson.';
+  // We concatenate strings using the "." character. ("+" is reserved for addition.)
+  $concattedString = $string1 . $string2;
+  // Difference between single and double quotes.
+  $mySingleQuoteHelloString = 'Hi there! My name is $string2';
+  // Double quotes are PARSED. PHP checks for variable names to echo out.
+  $myDoubleQuoteHelloString = "Howdy there! My name is $string2";
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $myMessage; ?></title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php echo $myMessage; // Echo is used for outputting strings. ?></title>
 </head>
 <body>
-    <h1><?php echo $myMessage; ?></h1>
+  <h1><?php echo $myMessage; ?></h1>
+  <?php include './includes/navigation.php'; ?>
+  <pre>
+    <?php
+      // Var dump is great for seeing what's inside!
+      var_dump( $myOtherArray );
+    ?>
+  </pre>
+  <h2>Concatenated String</h2>
+  <p>
+    <?php echo $concattedString; ?>
+  </p>
+  <h2>Difference Between Single and Double Quoted Strings</h2>
+  <h3>Single Quoted</h3>
+  <p>
+    <?php echo $mySingleQuoteHelloString; ?>
+  </p>
+  <h3>Double Quoted</h3>
+  <p>
+    <?php echo $myDoubleQuoteHelloString; ?>
+  </p>
 </body>
 </html>
