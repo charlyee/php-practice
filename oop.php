f2844f14f9a250f4e0ac6a2163cc430c262e439c
CharlZ <charles.ceo2020@gmail.com>
2020-04-30 11:10:24 -0600

    added oop file

---
+++ oop.php
@@ -0,0 +1,26 @@
+<?php
+    /**
+     * DEBUG error reporting!
+     * REMOVE BEFORE PUSHING TO PROD!
+     */
+    ini_set( 'display_errors', 1);
+    ini_set ( 'display_startup_errors', 1 );
+    error_reporting( E_ALL );
+    /**
+     * A basic object
+     */
+    $myObject = new stdClass(); // "new" keyword is required when making an object from a CLASS (or BLUEPRINT)
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP OOP</title>
+</head>
+<body>
+    <h1>PHP OOP</h1>
+    <?php include './includes/navigation.php' ?>
+    <h2>$myObject dump:</h2>
+    <pre><?php var_dump( $myObject ); ?></pre>
+</body>
+</html>
